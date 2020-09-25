0016c98f29cd7926d166d6b3a80d1f9581a1a20b
laurie kern <laurie.kern.ctr@us.af.mil>
2020-09-25 10:31:28 -0500

    Add project files.

---
+++ JenkinsTestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("press any key to close");
+             var tmp = Console.ReadLine();
+        }
+    }
+}
